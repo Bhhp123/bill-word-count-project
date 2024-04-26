2399d17e4968fb3bfa79de7e868346be94692d8a
Bilawal Hussain <bilawalhussain92@gmail.com>
2024-04-26 10:13:13 +0500

    update code

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+import { exit } from 'process';
+
+console.log('\n\t\t\t\x1b[44m======================================================\x1b[0m');
+console.log('\t\t\t\x1b[48;2;255;0;0m======== Bilawal Hussain\'s WORD COUNT PROJECT ========\x1b[0m');
+console.log('\t\t\t\x1b[44m======================================================\x1b[0m');
+
+const countWords = (text: string): number => {
+  return text.trim().split(/\s+/).length;
+};
+
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: '\x1b[34mEnter some text (or "exit" to quit):\x1b[0m',
+  },
+];
+
+const askForInput = async () => {
+  inquirer.prompt(questions).then((answers) => {
+    const text = answers.text;
+    if (text.toLowerCase() === 'exit') {
+      console.log('\t\x1b[31mExiting...\x1b[0m');
+      exit();
+    } else {
+      const wordCount = countWords(text);
+      console.log(`\t\x1b[32mWord count: ${wordCount}\x1b[0m`);
+      askForInput();
+    }
+  });
+};
+
+askForInput();
+
